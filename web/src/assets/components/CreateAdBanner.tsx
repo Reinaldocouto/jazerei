4394a6b4595a05cbf9148df218e68a04adcf94f1
Reinaldo Couto <43384617+Reinaldocouto@users.noreply.github.com>
2023-07-28 17:52:07 -0300

    first commit

---
+++ web/src/assets/components/CreateAdBanner.tsx
@@ -0,0 +1,21 @@
+import { MagnifyingGlassPlus } from "phosphor-react";
+import * as Dialog from '@radix-ui/react-dialog'
+
+export function CreateAdBanner () {
+    return (
+        <div className="pt-1 bg-gradient self-stretch rounded-lg mt-8 overflow-hidden">
+          <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
+            <div>
+              <strong className="text-2xl text-white font-black block">Zerou mais um?</strong>
+              <span className="text-zinc-400 block">Publique um anúncio para encontrar novos players!</span>
+            </div>
+
+          <Dialog.Trigger className="py-3 px-4 bg-cyan-300 hover:bg-cyan-500 text-gray-800 rounded flex items-center gap-3">
+            <MagnifyingGlassPlus size={24} />
+            <strong>Publicar anúncio</strong>
+          </Dialog.Trigger>
+
+          </div>
+        </div>
+    )
+}
---
+++ web/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx',
+    './index.html',
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif']
+    },
+    extend: {
+      backgroundImage: {
+        galaxy: "url('back730.png')",
+        'gradient': 'linear-gradient(60deg, #9572FC 25%, #43E7AD 30%, #E1D55D 70%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
+      },
+    },
+  },
+  plugins: [],
+}
+//
