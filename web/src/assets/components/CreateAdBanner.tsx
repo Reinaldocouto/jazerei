d742d024276ee472a4919e22f47718d9088a9960
Reinaldo Couto <43384617+Reinaldocouto@users.noreply.github.com>
2023-08-01 01:36:50 -0300

    Update new page

--- web/src/assets/components/CreateAdBanner.tsx
+++ web/src/assets/components/CreateAdBanner.tsx
@@ -3,10 +3,10 @@ import * as Dialog from '@radix-ui/react-dialog'
 
 export function CreateAdBanner () {
     return (
-        <div className="pt-1 bg-gradient self-stretch rounded-lg mt-8 overflow-hidden">
+        <div className="pt-1 bg-gradient self-stretch rounded-lg mt-0 overflow-hidden">
           <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
             <div>
-              <strong className="text-2xl text-white font-black block">Zerou mais um?</strong>
+              <strong className="text-2xl text-white font-black block">Procurando um duo?</strong>
               <span className="text-zinc-400 block">Publique um anúncio para encontrar novos players!</span>
             </div>
 
--- web/src/assets/components/CreateAdPage.tsx
+++ web/src/assets/components/CreateAdPage.tsx
@@ -7,7 +7,7 @@ export function CreateAdPage () {
           <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
             <div>
               <strong className="text-2xl text-white font-black block">Zerou mais um?</strong>
-              <span className="text-zinc-400 block">Publique um anúncio para encontrar novos players!</span>
+              <span className="text-zinc-400 block">Registra aqui tudo o que você achou do game!</span>
             </div>
 
           <Dialog.Trigger className="py-3 px-4 bg-cyan-300 hover:bg-cyan-500 text-gray-800 rounded flex items-center gap-3">
