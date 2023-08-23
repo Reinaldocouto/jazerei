17047841d9afb1f79958972210582d1584f8f773
Reinaldo Couto <43384617+Reinaldocouto@users.noreply.github.com>
2023-08-23 15:43:42 -0300

    Update tailwind.config.js

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
     },
     extend: {
       backgroundImage: {
-        galaxy: "url('/back730.png')",
+        galaxy: "url('back730.png')",
         'gradient': 'linear-gradient(60deg, #9572FC 25%, #43E7AD 30%, #E1D55D 70%)',
         'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
       },
